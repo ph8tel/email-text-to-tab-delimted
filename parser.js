d583102b13670af9ebc2ca112ef1c495490a5698
Matt Weber <mwebersm@gmail.com>
2018-06-22 09:53:12 -0800

    Create example email list with various edge cases

---
+++ parser.js
@@ -0,0 +1,3 @@
+// Create array with values.
+
+var emailList = ['            Matt Weber <matt@gmail.com>, Kira Weber <kira@gmail.com>, "Vera Savich, MA, LMFT" <vera@hotmail.com>, 831Shooter <831shooter@yahoo.com>, <test@att.net>, "(null) (null)" <no@gmail.com>      '];
