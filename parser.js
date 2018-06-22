2264b6e38a2145d1354e7b9d52a454b6f2f372bd
Matt Weber <mwebersm@gmail.com>
2018-06-22 10:35:51 -0800

    Remove spaces I added for readability

--- parser.js
+++ parser.js
@@ -1,6 +1,6 @@
 // Create array with values.
 
-var emailList = ['            Matt Weber <matt@gmail.com>, Kira Weber <kira@gmail.com>, "Vera Savich, MA, LMFT" <vera@hotmail.com>, 831Shooter <831shooter@yahoo.com>, <test@att.net>, "(null) (null)" <no@gmail.com>      '];
+var emailList = ['Matt Weber <matt@gmail.com>, Kira Weber <kira@gmail.com>, "Vera Savich, MA, LMFT" <vera@hotmail.com>, 831Shooter <831shooter@yahoo.com>, <test@att.net>, "(null) (null)" <no@gmail.com>'];
 
 function parseList(insaneList) {
   var saneList = '';
