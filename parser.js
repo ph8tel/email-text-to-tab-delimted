b4ad6382535c026a920194b40c582ef67ca7a44b
Matt Weber <mwebersm@gmail.com>
2018-06-22 10:48:05 -0800

    refactored again since I no longer need to return anything

--- parser.js
+++ parser.js
@@ -3,8 +3,6 @@
 var emailList = ['Matt Weber <matt@gmail.com>, Kira Weber <kira@gmail.com>, "Vera Savich, MA, LMFT" <vera@hotmail.com>, 831Shooter <831shooter@yahoo.com>, <test@att.net>, "(null) (null)" <no@gmail.com>'];
 
 function parseList(insaneList) {
-  var saneList = '';
-
   // Iterate over the charcters of the list
     // Remove any quotations or paretheses
   // Split by Commas
@@ -21,8 +19,6 @@ function parseList(insaneList) {
         // Join all array elements before last to make name with a space
         // iterate over list and remove any null elements
         // Console log the first eleement + tab + email + new line
-
-  return saneList;
 }
 
 /*
