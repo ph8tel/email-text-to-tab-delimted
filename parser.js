164d94dade75211449791b9f4e8bf39996dbe9fb
Matt Weber <matt@weberdesign-ak.com>
2018-06-22 12:21:48 -0800

    Remove duplicated function

--- parser.js
+++ parser.js
@@ -4,25 +4,6 @@
 
 var emailList = 'Matt Weber <matt@gmail.com>, Kira Weber <kira@gmail.com>, "Vera Savich, MA, LMFT" <vera@hotmail.com>, 831Shooter <831shooter@yahoo.com>, "(null) (null)" <no@gmail.com>';
 
-function parseList(insaneList) {
-  saneList = insaneList.split('"')
-                         .join('')
-                         .split(',')
-                         .join('')
-                         .split('>');
-
-  for (var i = 0; i < saneList.length; i++) {
-    var person = saneList[i].split('<');
-    if (person[0][0] === ' ') {
-      person[0] = person[0].substring(1);
-    }
-    console.log(person[0] + '\t' + person[1]);
-  }
-}
-
-parseList(emailList);
-
-
 function parseList(insaneList, needFirstLast) {
   saneList = insaneList.split('"')
                          .join('')
